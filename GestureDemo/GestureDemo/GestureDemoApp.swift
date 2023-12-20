bb40436550db7429b527bbfff88d8d8da2073f0e
Jihunkim95 <darak551@naver.com>
2023-12-20 09:32:09 +0900

    add: init

---
+++ GestureDemo/GestureDemo/GestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureDemoApp.swift
+//  GestureDemo
+//
+//  Created by 김지훈 on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
