1f801c4f08efb2aa38c1651f130b1edb2059e392
Jihunkim95 <darak551@naver.com>
2023-12-21 10:37:33 +0900

    add: gesture 및 코드 재사용성

--- GestureDemo/GestureDemo/ContentView.swift
+++ GestureDemo/GestureDemo/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        // 코드의 재사용성
+        let tap = TapGesture()
+            .onEnded{ _ in
+                print("Tapped")
+            }
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "hand.point.right.fill")
+                .gesture(tap)
         }
         .padding()
+        
+        
     }
 }
 
